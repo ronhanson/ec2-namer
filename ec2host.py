bdf33422ee3d75b38ad6523b08b72b10f081a47f
Ronan Delacroix <ronan.delacroix@gmail.com>
2018-08-02 16:15:19 +0200

    Refactoring and some behavior changes (v0.4.0)

--- ec2host.py
+++ ec2host.py
@@ -23,9 +23,9 @@ def check_ec2_hostname_tags():
     group = tags.get('group')
     number = tags.get('number', "0001")
     env = tags.get('environment', None)
-    private_hostname = tags.get('private-hostname', None)
-    public_hostname = tags.get('public-hostname', None)
-    service_name = tags.get('service-name', None)
+    suffix = '.' + str(env)
+    if not env or env == 'prod' or env == 'production':
+        suffix = ''
 
     if not group or not private_zone:
         logging.error('No group or zone tag found for instance %s' % instance.id)
@@ -33,62 +33,70 @@ def check_ec2_hostname_tags():
 
     logging.info("Current instance tags : %s" % (', '.join(['%s=%s' % (str(k), str(v)) for k, v in tags.items()])))
 
-    instances_like_me = ec2.get_instances_by_tags(tags={
+    filters = {
         'private-zone': private_zone,
         'group': group,
-        'number': number,
-        'environment': env
-    })
+        'number': number
+    }
+    if suffix:
+        filters['environment'] = env
+    instances_like_me = ec2.get_instances_by_tags(tags=filters)
 
-    other_instances = [inst for inst in instances_like_me if inst.id != instance.id]
-    if len(other_instances) >= 1 or not private_hostname or not public_hostname or not service_name:
+    other_instances = [
+        inst for inst in instances_like_me
+        if inst.id != instance.id and inst.state.get('Name', 'running') == 'running'
+    ]
+
+    if len(other_instances) >= 1:
         logging.warning("Found %d instances having same group and number (%s). Updating EC2 tags." % (
             len(other_instances), ', '.join([i.id for i in other_instances])
         ))
 
         # found another instance like me, so I am going to change tags and edit my name
-        group_instances = ec2.get_instances_by_tags(tags={
-            'group': group,
+        filters = {
             'private-zone': private_zone,
-            'environment': env
-        })
-        group_instances_numbers = [ec2.get_instance_tags(inst).get('number') for inst in group_instances if
-                                   inst.id != instance.id]
+            'group': group
+        }
+        if suffix:
+            filters['environment'] = env
+        group_instances = ec2.get_instances_by_tags(tags=filters)
+        group_instances_numbers = [
+            ec2.get_instance_tags(inst).get('number', 1000)
+            for inst in group_instances
+            if inst.id != instance.id and inst.state.get('Name', 'running') == 'running'
+        ]
         group_instances_numbers = sorted(group_instances_numbers)
         new_lowest_available_number = None
         for i in range(1, 999):
             if "%04d" % i not in group_instances_numbers:
                 new_lowest_available_number = "%04d" % i
                 break
-        suffix = '.' + env
-        if env == 'prod' or env == 'production':
-            suffix = ''
-        public_hostname = "{}-{}{}".format(group, new_lowest_available_number, suffix)
-        private_hostname = public_hostname.replace('.', '-')
-        group_dns = "{}{}".format(group, suffix)
-        new_tags = {
-            'group': group,
-            'number': new_lowest_available_number,
-            'public-zone': public_zone,
-            'private-zone': private_zone,
-            'environment': env,
-            'private-hostname': private_hostname,
-            'public-hostname': public_hostname,
-            'service-name': group_dns,
-            'Name': "{}.{}".format(public_hostname, public_zone)
-        }
-        logging.info("New instance tags : %s" % (', '.join(['%s=%s' % (str(k), str(v)) for k, v in new_tags.items()])))
-        # Update tags of current instance
-        ec2.create_tags(instance, tags=new_tags)
+        number = new_lowest_available_number
 
-        instance.reload()
-        logging.info("Instance %s updated" % str(instance.id))
-        return True
-    else:
-        return False
+    public_hostname = "{}-{}{}".format(group, number, suffix)
+    private_hostname = public_hostname.replace('.', '-')
+    new_tags = {
+        'number': number,
+        'Name': "{}.{}".format(public_hostname, public_zone)
+    }
+
+    # Update tags of current instance
+    ec2.create_tags(instance, tags=new_tags)
+
+    logging.info("Instance %s updated" % str(instance.id))
+
+    instance.reload()
+    updated_tags = ec2.get_instance_tags(instance)
+    logging.info("New instance tags : %s" % (', '.join(['%s=%s' % (str(k), str(v)) for k, v in updated_tags.items()])))
+
+    return private_hostname
 
 
 def create_dns_record(r53, zone, dns, ip_address):
+
+    if type(ip_address) is list:
+        ip_address = [ip for ip in ip_address if ip]  # filter None if multiple IP addresses given as param
+
     zone_id = r53.get_zone_id(name=zone)
 
     dns_record_name = dns + '.' + zone + '.'
@@ -107,53 +115,68 @@ def create_public_routes():
     logging.info("Ensuring public routes for instance %s" % str(instance.id))
 
     public_zone = tags.get('public-zone', None)
-    public_hostname = tags.get('public-hostname', None)
-
     private_zone = tags.get('private-zone', None)
-    private_hostname = tags.get('private-hostname', None)
+    number = tags.get('number', None)
+    group = tags.get('group', None)
+    env = tags.get('environment', None)
+    suffix = '.' + str(env)
+    if not env or env == 'prod' or env == 'production':
+        suffix = ''
+
+    if not group or not number:
+        logging.error('No group or number tag found for instance %s' % instance.id)
+        return None
+
+    public_hostname = "{}-{}{}".format(group, number, suffix)
+    private_hostname = public_hostname.replace('.', '-')
+    service_name = "{}{}".format(group, suffix)
 
     r53 = tbx.aws.Route53()
 
-    create_dns_record(r53, private_zone, private_hostname, instance.private_ip_address)
-    create_dns_record(r53, public_zone, public_hostname, instance.public_ip_address)
+    if private_zone:
+        create_dns_record(r53, private_zone, private_hostname, instance.private_ip_address)
+        ec2.create_tags(instance, tags={'private-dns': "{}.{}".format(private_hostname, private_zone)})
 
-    ec2.create_tags(instance, tags={
-        'public-dns': "{}.{}".format(public_hostname, public_zone),
-        'private-dns': "{}.{}".format(private_hostname, private_zone)
-    })
+    if public_zone:
+        create_dns_record(r53, public_zone, public_hostname, instance.public_ip_address)
+        ec2.create_tags(instance, tags={'public-dns': "{}.{}".format(public_hostname, public_zone)})
+
     instance.reload()
     tags = ec2.get_instance_tags(instance)
 
     ##
     # Create / Update service dns entries
     ##
-    service_name = tags.get('service-name', None)
+    filters = {
+        'private-zone': private_zone,
+        'group': group
+    }
+    if suffix:
+        filters['environment'] = env
+    instances_same_group = ec2.get_instances_by_tags(tags=filters)
 
-    if service_name:
-        group = tags.get('group')
-        env = tags.get('environment', None)
-
-        instances_same_group = ec2.get_instances_by_tags(tags={
-            'private-zone': private_zone,
-            'group': group,
-            'environment': env
-        })
-
-        public_ip_adresses = [(int(ec2.get_instance_tags(i).get('number', 1000)), i.public_ip_address) for i in instances_same_group]
-        private_ip_adresses = [(int(ec2.get_instance_tags(i).get('number', 1000)), i.private_ip_address) for i in instances_same_group]
-
-        public_ip_adresses = [ip for i, ip in sorted(public_ip_adresses, key=itemgetter(0))]
-        private_ip_adresses = [ip for i, ip in sorted(private_ip_adresses, key=itemgetter(0))]
-
-        create_dns_record(r53, public_zone, service_name, public_ip_adresses)
+    if private_zone:
+        private_ip_adresses = [
+            (int(ec2.get_instance_tags(i).get('number', 1000)), i.private_ip_address)
+            for i in instances_same_group
+            if i.private_ip_address and i.state.get('Name', 'running') == 'running'
+        ]
+        private_ip_adresses = [ip for i, ip in sorted(private_ip_adresses, key=itemgetter(0)) if ip]
         create_dns_record(r53, private_zone, service_name, private_ip_adresses)
+        ec2.create_tags(instance, tags={'private-service-dns': "{}.{}".format(service_name, private_zone)})
 
-        ec2.create_tags(instance, tags={
-            'public-service-dns': "{}.{}".format(service_name, public_zone),
-            'private-service-dns': "{}.{}".format(service_name, private_zone)
-        })
-        instance.reload()
-        tags = ec2.get_instance_tags(instance)
+    if public_zone:
+        public_ip_adresses = [
+            (int(ec2.get_instance_tags(i).get('number', 1000)), i.public_ip_address)
+            for i in instances_same_group
+            if i.public_ip_address and i.state.get('Name', 'running') == 'running'
+        ]
+        public_ip_adresses = [ip for i, ip in sorted(public_ip_adresses, key=itemgetter(0)) if ip]
+        create_dns_record(r53, public_zone, service_name, public_ip_adresses)
+        ec2.create_tags(instance, tags={'public-service-dns': "{}.{}".format(service_name, public_zone)})
+
+    instance.reload()
+    tags = ec2.get_instance_tags(instance)
 
     return tags
 
